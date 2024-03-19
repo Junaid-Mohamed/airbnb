6d35183a04ee53b7910b51ee1391f875cf10b38a
Mohammed Junaid <junaid7mohamed@gmail.com>
2024-03-19 22:58:57 +0530

    added bsae structure for the modal

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Inter, Nunito } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/navbar/Navbar";
+import ClientOnly from "./components/ClientOnly";
+import Modal from "./components/modals/Moda";
 
 
 
@@ -22,7 +24,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
+        <ClientOnly>
         <Navbar/>
+        <Modal isOpen/>
+        </ClientOnly>
+       
         {children}
         </body>
     </html>
--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,6 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-   <div className="text-rose-500 p-20"  >Hi There!!</div>
+   <div className="text-rose-500 p-20"  ></div>
   );
 }
